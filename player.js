dc38129da3390a2c305bd1d62520f43df01d2d26
Eduardo Domene Junior <eduardo.djr@hotmail.com>
2019-10-26 23:56:51 +0200

    Initial commit. Dots are being displayed in the screen.

---
+++ player.js
@@ -0,0 +1,65 @@
+var matrix = createMatrix();
+createDebug(matrix);
+createMatrixOnCanvas();
+
+function createMatrixOnCanvas() {
+  var main = document.getElementById("myCanvas");
+  var ctx = main.getContext("2d");
+  
+  let numberOfElements = 17;
+  let canvasHeight = main.offsetHeight;
+  let canvasWidth = main.offsetWidth;
+  let elementWidth = canvasWidth / numberOfElements;
+  let xOffset = yOffset = elementWidth/2;
+  
+  var xPosition = xOffset;
+  var yPosition = canvasHeight - yOffset;
+  
+  for (var i = 0; i < numberOfElements; i++) {
+    createCircleAt(xPosition, yPosition);
+    xPosition += elementWidth;
+  }
+}
+
+function createCircleAt(x, y) {
+  var main = document.getElementById("myCanvas");
+  var ctx = main.getContext("2d");
+  
+  let modifier = 0.5;
+  let canvasWidth = main.offsetWidth;
+  let elementWidth = canvasWidth / 17;
+  let elementRadius = elementWidth/2 * modifier;
+
+  
+  ctx.beginPath();
+  ctx.arc(x, y, elementRadius, 0, 2 * Math.PI);
+  ctx.stroke();
+}
+
+function createDebug(matrix) {
+  var main = document.getElementById("main");
+  main.innerHTML += prettyText(matrix);
+}
+
+function createMatrix() {
+    var line = new Array(4)
+    
+    for (var i = 0; i < line.length; i++) {
+        line[i] = new Array(17);
+      
+        for (var j = 0; j < line[i].length; j++) {
+            line[i][j] = 0;
+        }
+    }
+    
+    return line;
+}
+
+// Helpers
+function prettyText(matrix) {
+  var text = "";
+  for (var i = 0; i < matrix.length; i++) {
+    text += matrix[i]+"</br>";
+  }
+  return text
+}
